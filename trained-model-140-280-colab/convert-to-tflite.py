a2340d7ad009544a5cfe226b660275900972e87b
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2019-06-13 17:33:52 +0530

    remove unnecessary models

--- trained-model-140-280-colab/convert-to-tflite.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-converter = tf.lite.TFLiteConverter.from_keras_model_file('model-140-v2-7200.h5')
-tflite_model = converter.convert()
-open('cnn-model.tflite', 'wb').write(tflite_model)
